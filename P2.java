496b56db2df4b1f51697787e389f58b5dc26237f
sahersandhu <Sahernadeem304@gmail.com>
2020-11-29 17:51:16 +0500

    2

---
+++ P2.java
@@ -0,0 +1,12 @@
+public class P2 {  
+public static void main(String[] args)
+ {  
+     
+    int age=20;  
+    
+    if(age>18)
+{  
+        System.out.printIn("Age is greater than 18");  
+    }  
+}  
+}  
