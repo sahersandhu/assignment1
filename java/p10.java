0c9942ee30970aad3cb08e04851568b4f8abee81
sahersandhu <Sahernadeem304@gmail.com>
2020-11-29 17:26:45 +0500

    3assignment1

---
+++ java/p10.java
@@ -0,0 +1,12 @@
+class p10{
+ public static void main(String args[]){
+	int a=10;
+	int b=20;
+	int z;
+	z=a+b;
+	System.out.println("a+b="+z);
+
+
+	}
+
+}
---
+++ p1.java
@@ -0,0 +1,12 @@
+public class P1
+ {
+   public static void main(String[] args)
+   {
+     int x = 3;
+     int y = 2;
+     int z =x*y;
+     System.out.println(z);
+     
+   }
+ }
+ 
